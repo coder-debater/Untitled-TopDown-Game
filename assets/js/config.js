a030497875f4f3337433a23c736f605d13bffa6a
wensenfriendandextra <wensen.dong@gmail.com>
2023-04-04 17:17:36 +1000

    fix: stopTime not in config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -6,3 +6,7 @@
 //* 'PNG' or 'SVG'
 //* Does not apply to player image
 const IMAGE_TYPE = 'PNG';
+
+//* Time until animation stops in milliseconds
+//* Affects render, unrender, start, stop
+const stopTime = 0;
