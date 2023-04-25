6bb2714594a90b58a3a2b4b4b814c8d72ede00e0
Wansen Dong <wensen.dong@gmail.com>
2023-04-25 13:26:47 +1000

    feat: remove scripts moved to movePlayer.js

--- assets/js/render.js
+++ assets/js/render.js
@@ -18,33 +18,6 @@ function s(x) {
 
 
 
-// Player
-
-// Position on land
-let playerTilePos = {"x": 0, "y": 14};
-// Which land are we in?
-let playerLandPos = {"x": 0, "y": 0};
-
-// Check position to position equivalence
-function equivPos(a, b) {
-    return (a.x === b.x && a.y === b.y);
-}
-
-// Check position to X-Y equivalence
-function equivXY(a, x, y) {
-    return (a.x === x && a.y === y);
-}
-
-function getPlayerLand() {
-    for (let land of Object.values(map)) {
-        if (equivPos(land.landPos, playerLandPos)) {
-            return land;
-        }
-    }
-}
-
-
-
 // Tiles
 
 // Size of one tile
